a1893db67d60e83c8573f841d868127e76648c55
khlstudy <145128666+nayavua@users.noreply.github.com>
2024-07-05 18:00:37 +0300

    Add project files.

---
+++ TestT/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TestT
+{
+    partial class AnalyzeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBrowse.Location = new System.Drawing.Point(162, 27);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(134, 50);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Вибрати файл";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnAnalyze.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAnalyze.Location = new System.Drawing.Point(162, 140);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(134, 50);
+            this.btnAnalyze.TabIndex = 1;
+            this.btnAnalyze.Text = "Аналізувати числа";
+            this.btnAnalyze.UseVisualStyleBackColor = false;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(12, 196);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.Size = new System.Drawing.Size(458, 545);
+            this.txtResults.TabIndex = 2;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 112);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(458, 22);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblPath.Location = new System.Drawing.Point(12, 93);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(39, 16);
+            this.lblPath.TabIndex = 4;
+            this.lblPath.Text = "Шлях";
+            // 
+            // AnalyzeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(482, 753);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "AnalyzeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Аналіз чисел";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label lblPath;
+    }
+}
+
---
+++ TestT/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TestT
+{
+    public partial class AnalyzeForm : Form
+    {
+        public AnalyzeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFilePath.Text) || !File.Exists(txtFilePath.Text))
+            {
+                MessageBox.Show("Будь ласка виберіть коректний файл.");
+                return;
+            }
+
+            try
+            {
+                var numbers = File.ReadAllLines(txtFilePath.Text).SelectMany(line => line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)).Select(int.Parse).ToArray();
+
+                if (numbers.Length == 0)
+                {
+                    MessageBox.Show("Файл порожній або містить некоректні значення.");
+                    return;
+                }
+
+                int max = numbers.Max();
+                int min = numbers.Min();
+                double mean = numbers.Average();
+
+                Array.Sort(numbers);
+                double median = numbers.Length % 2 == 0 ? (numbers[numbers.Length / 2 - 1] + numbers[numbers.Length / 2]) / 2.0 : numbers[numbers.Length / 2];
+
+                var increasingSeq = FindLongestSequence(numbers, (a, b) => a < b);
+                var decreasingSeq = FindLongestSequence(numbers, (a, b) => a > b);
+
+                txtResults.Text = $"Максимум: {max}\r\nМінімум: {min}\r\nСереднє арифметичне: {mean}\r\nМедіана: {median}\r\nНайдовша зростаюча послідовність: {string.Join(", ", increasingSeq)}\r\nНайдовша спадаюча послідовність: {string.Join(", ", decreasingSeq)}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Виникла помилка: {ex.Message}");
+            }
+        }
+
+        private static int[] FindLongestSequence(int[] arr, Func<int, int, bool> compareFunc)
+        {
+            var longestSeq = new int[0];
+            var currentSeq = new int[arr.Length];
+            int currentLength = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == 0 || compareFunc(arr[i - 1], arr[i]))
+                {
+                    currentSeq[currentLength++] = arr[i];
+                }
+                else
+                {
+                    if (currentLength > longestSeq.Length)
+                    {
+                        longestSeq = currentSeq.Take(currentLength).ToArray();
+                    }
+                    currentSeq[0] = arr[i];
+                    currentLength = 1;
+                }
+            }
+
+            if (currentLength > longestSeq.Length)
+            {
+                longestSeq = currentSeq.Take(currentLength).ToArray();
+            }
+
+            return longestSeq;
+        }
+    }
+}
